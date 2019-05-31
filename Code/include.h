7c270ab09812d1770bbe4fa065b90de49b56ff1e
huangshuang <853071569@qq.com>
2019-05-31 19:22:39 +0800

    完成include.h文件编写

---
+++ Code/include.h
@@ -0,0 +1,14 @@
+/**----------------------------------------------------------**/
+/**	File name : include.h									 **/
+/**	Author	  : Huangshuang 								 **/
+/**	Data 	  : 2019/5/31									 **/
+/**	Vertion	  : V1.0										 **/
+/**----------------------------------------------------------**/
+
+#ifndef _INCLUDE_H_
+#define _INCLUDE_H_
+
+#include "stm32f10x.h"
+#include "math.h"
+
+#endif
