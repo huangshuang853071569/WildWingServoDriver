b808695a677a5ee599d5131ec101f7c4775065dc
huangshuang <853071569@qq.com>
2019-06-03 18:22:45 +0800

    修改死区控制

--- Code/pid/pid.c
+++ Code/pid/pid.c
@@ -4,7 +4,7 @@
 /**	Data	  : 2019/5/31										**/
 /**	Version	  : V1.0											**/
 /**-------------------------------------------------------------**/
-
+#include "usart.h"
 #include "pid.h"
 
 float err_ext;			//外环误差值
@@ -14,7 +14,7 @@ float out_ext;			//外环输出值
 float obs_ext_last;		//上一次的外环观测值*
 
 float err_ins;			//内环误差值
-float obs_ins			//内环观测值
+float obs_ins;			//内环观测值
 float exc_ins;			//内环期望值
 float out_ins;			//内环输出值	*
 	
@@ -37,7 +37,7 @@ float pid_control(float except,float current,float delta_T)
 
   /**----------------------------Extern Control----------------------------**/
 	err_ext	= exc_ext - obs_ext;					//求外环误差 
-	if(fabs(err_ext)<POS_DEAD)err_ext=0;			//死区
+	if(fabs(err_ext)<200)err_ext=0;				//死区
 	out_ext = err_ext * KP_EXT;						//求外环输出
   /**----------------------------------------------------------------------**/
   
@@ -48,13 +48,15 @@ float pid_control(float except,float current,float delta_T)
 	
   /**----------------------------Inside Control----------------------------**/
 	err_ins = exc_ins - obs_ins;					//求内环误差  														
-															
-	if(												//抗饱和积分
-	   ( (out_ins > PID_MAX)&&(err_ins > 0) ) || 
-	   ( (out_ins < PID_MIN)&&(err_ins < 0) ) )	 
-	{}
-	else 
-		integral_ins+=err_ins;
+	//if(fabs(err_ins)<300)err_ins=0;
+	
+//	if(																		//抗饱和积分
+//	   ( (out_ins > PID_MAX)&&(err_ins > 0) )||( (out_ins < PID_MIN)&&(err_ins < 0) ) )	 
+//	{
+//		printf("here! \r\n");												
+//	}
+//	else 
+	{integral_ins=integral_ins+err_ins;}
 	
 	#ifdef ENABLE_INTEGRAL_SEPARATING				//如果使能了积分分离
 	{
@@ -87,15 +89,8 @@ float pid_control(float except,float current,float delta_T)
 	obs_ext_last = obs_ext;							//保存本次外环观测值
 	
 	pid_out	= out_ins;
+	if((pid_out<22000)&&(pid_out>1500))pid_out=16000;
+	if((pid_out>-22000)&&(pid_out<-1500))pid_out=-16000;
+	if(fabs(pid_out)<=1500)pid_out=0;
 	return pid_out;
 }
-
-
-
-
-
-
-
-
-
-
--- Code/pid/pid.h
+++ Code/pid/pid.h
@@ -8,28 +8,28 @@
 #ifndef _PID_H
 #define	_PID_H
 
-/*Including Here
+/*Including Here*/
 /*------------------*/
 #include "stm32f10x.h"
 #include "math.h"
 /*------------------*/
 
-#define ENABLE_INTEGRAL_SEPARATING		//是否使能积分分离（定义则使能）
+//#define ENABLE_INTEGRAL_SEPARATING		//是否使能积分分离（定义则使能）
 
 /*extern parameters*/			//外环参数
-#define KP_EXT 5.0f
+#define KP_EXT 0.1f
 #define KI_EXT 0.0f
 #define KD_EXT 0.0f
 
 /*inside parameters*/			//内环参数
-#define KP_INS 3.0f
-#define KI_INS 0.5f
+#define KP_INS 35.0f
+#define KI_INS 0.0f
 #define KD_INS 2.0f
 
-#define PID_MAX 		100			//积分上限
-#define PID_MIN 		-100		//积分下限
-#define ERR_TRD_INS 	50			//积分分离的误差阈值
-#define	POS_DEAD 		150			//死区
+#define PID_MAX 			20000			//积分上限
+#define PID_MIN 			-20000		//积分下限
+#define ERR_TRD_INS 	500		//积分分离的误差阈值
+#define	POS_DEAD 			1		//误差死区
 
 float pid_control(float except,float current,float delta_T);
 #endif
